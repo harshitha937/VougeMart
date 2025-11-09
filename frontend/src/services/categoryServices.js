f75da5d7d627673ca945c850c242fd3379696efb
harshitha937 <valluruharshitha2005@gmail.com>
2025-11-10 00:23:09 +0530

    vercel backend

--- frontend/src/services/categoryServices.js
+++ frontend/src/services/categoryServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = 'http://localhost:5000/categories';
+const API = 'https://vougemart-1.onrender.com/categories';
 
 export const getAllCategories = () => axios.get(API);
